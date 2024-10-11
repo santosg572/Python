161c75bf931944247dea1c30bbcb5d33eb9bccf6
santosg572 <santosg572@gmail.com>
2024-10-11 12:11:50 -0600

    Test comité

---
+++ Python_Buscar/buscar.py
@@ -0,0 +1,14 @@
+# importar la funcion de busqueda
+from googlesearch import search
+# esta variable contiene el paramentro o consulta de busqueda
+
+q = "parkinson"
+
+# ahora ejecutamos la busqueda con la funcion search y pasamos como parametro la consulta
+
+results = search(q, num_results=100)
+
+# hacemos un recorrido de los resultados, cada resultado es una URL
+for r in results:
+	print(r)
+
