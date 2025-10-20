15c6ef0a091b008bdba6495cb602b0e8dea67dbd
santosg572 <santosg572@gmail.com>
2025-10-20 12:19:21 -0600

    Test comité

--- Python_Tkinter/p1.py
+++ Python_Tkinter/p1.py
@@ -1,9 +1,8 @@
 import tkinter
-
 window = tkinter.Tk()
 # to rename the title of the window
 window.title("GUI")
 # pack is used to show the object in the window
-label = tkinter.Label(window, text = "Welcome to DataCamp's Tutorial on Tkinter!").pack()
+#label = tkinter.Label(window, text = "Welcome to DataCamp's Tutorial on Tkinter!").pack()
 window.mainloop()
 
