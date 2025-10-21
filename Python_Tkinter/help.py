710708ae2b9aa87bdb1b8e4eba672b60790264c2
santosg572 <santosg572@gmail.com>
2025-10-21 16:09:49 -0600

    Test comité

--- Python_Tkinter/help.py
+++ Python_Tkinter/help.py
@@ -1,6 +1,16 @@
 import tkinter
 window = tkinter.Tk()
 # to rename the title of the window
+
+dd = dir(tkinter)
+
+for ss in dd:
+   if ss[0] != '_':
+     print(help(eval('tkinter.'+ss)))
+
+
+
+'''
 window.title("GUI")
 b1 = tkinter.Button(window,text="Hola buenos días")
 b1.pack()
@@ -10,3 +20,4 @@ e1 = tkinter.Entry()
 l1 = tkinter.Label()
 print(help(window.grid))
 
+'''
