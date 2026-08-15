5e333b6115488182b2ba6978ef5f5dba9283b29f
santosg572 <santosg572@gmail.com>
2026-08-15 03:22:58 -0600

    Test comité

---
+++ help/p1.py
@@ -0,0 +1,16 @@
+import numpy 
+import matplotlib.pyplot
+
+# help()
+
+#help('import')
+
+#help('math')
+
+#help('numpy')
+
+#help('modules')
+
+#help('matplotlib.pyplot')
+
+help('open')
---
+++ help/p2.py
@@ -0,0 +1,21 @@
+import numpy 
+import matplotlib.pyplot
+
+import sys
+
+# help()
+
+#help('import')
+
+#help('math')
+
+#help('numpy')
+
+#help('modules')
+
+#help('matplotlib.pyplot')
+#dd = 'open'
+
+dd = sys.argv[1]
+
+help(dd)
