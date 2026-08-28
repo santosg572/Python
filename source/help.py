dd9b970769175b264625f2e0f0679dc6ed898e63
santosg572 <santosg572@gmail.com>
2026-08-28 05:09:07 -0600

    Test comité

--- source/help.py
+++ source/help.py
@@ -1,10 +1,14 @@
-import numpy as np
-import math
-import matplotlib.pyplot as plt
+import tkinter as tk
 
-dd =['atan', 'ceil', 'comb', 'cos', 'degrees', 'dist', 'e', 'exp', 'fabs', 'factorial', 'floor', 'fma', 'fmod', 'frexp', 'fsum', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'log', 'log10', 'modf', 'nan', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sqrt', 'tan', 'tau', 'trunc']
+#root = tk.Tk()
+
+dd = dir(tk)
 
 for ss in dd:
-  print(help('math.'+ss))
+  if ss[0] != '_':
+    print("%%%%%%%%%%%%%%%%%%%%% " + ss + " %%%%%%%%%%%%%%%%%%%%%")
+    print(help(eval('tk.'+ss)))
+
+
 
 
